39c3a37cec80c2399d7f8d030a09f5c8c45876d3
Andrew Souhrada <ajsouhrada@NN123-HA-07.HS.sd203.org>
2015-08-20 15:08:28 -0500

    Exploring Turtle
    Made multi-colored smiley face. :)

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,27 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
         turtle.penDown();
-        turtle.forward(50);
+        turtle.forward(100);
+        turtle.penUp();
+        turtle.turn(90);
+        turtle.forward(100);
+        turtle.turn(90);
+        turtle.penDown();
+        turtle.forward(100);
+        turtle.penUp();
+        turtle.setPenWidth(10);
+        turtle.setPenColor(Color.GREEN);
+        turtle.turn(25);
+        turtle.forward(35);
+        turtle.turn(20);
+        turtle.penDown();
+        turtle.forward(30);
+        turtle.turn(45);
+        turtle.forward(30);
+        turtle.turn(45);
+        turtle.forward(30);
+        turtle.penUp();
     }
 }
